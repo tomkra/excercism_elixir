4b90e56fc447df7da9104052d4ef0c3dc86e6085
Tomáš Král <kralutomas@gmail.com>
2023-11-01 18:56:25 +0100

    leap year

---
+++ leap/lib/year.ex
@@ -0,0 +1,18 @@
+defmodule Year do
+  @doc """
+  Returns whether 'year' is a leap year.
+
+  A leap year occurs:
+
+  on every year that is evenly divisible by 4
+    except every year that is evenly divisible by 100
+      unless the year is also evenly divisible by 400
+  """
+  @spec leap_year?(non_neg_integer) :: boolean
+  def leap_year?(year) do
+    rem100 = rem(year, 100) == 0
+    rem400 = rem(year, 400) == 0
+
+    rem(year, 4) == 0 && ((!rem100 && !rem400) || (rem100 && rem400))
+  end
+end
